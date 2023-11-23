e19a9bb9096cb3fb3d23c2deedcbcd9172ef2a66
ginoya <rishitginoya@gmail.com>
2023-11-23 09:15:20 +0530

    Initial commit

---
+++ Assignment9/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Assignment9.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [LoggerFilter]
+        public string TestFilter(string testFilter, int a)
+        {
+            Console.WriteLine("Inside Home Controller");
+            return $"Hello World - {testFilter} - {a}";
+        }
+    }
+}
---
+++ Assignment9/Filters/LoggerFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Assignment9.Filters
+{
+    public class LoggerFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            foreach (var parameter in context.ActionArguments)
+            {
+                Console.WriteLine($"Inside Filter OnActionExecuting - {parameter.Key}: {parameter.Value}");
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine("Inside Filter OnActionExecuted");
+        }
+    }
+}
